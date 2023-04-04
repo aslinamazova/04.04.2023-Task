e692f3c197ff919eec51d8535cd38fec8dfc1cc7
aslinamazova <76001yw@code.edu.az>
2023-04-04 09:28:06 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int i = 18;
+if (i > 10 && i < 20)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine("Cut ededdir");
+    }
+    else if (i % 2 != 0)
+    {
+        Console.WriteLine("Tek ededdir");
+    }
+}
+else
+{
+    Console.WriteLine("10-la 20 arasi eded deyil");
+}
+
